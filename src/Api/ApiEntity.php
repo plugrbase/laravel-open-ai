4636133f13ace9dfd77a38c1b1f4f56045d6096b
Julien Legrand <julien@nomala.com>
2022-01-21 15:45:07 +0100

    Store engine types withing enum

--- src/Api/ApiEntity.php
+++ src/Api/ApiEntity.php
@@ -2,8 +2,15 @@
 
 namespace Plugrbase\OpenAi\Api;
 
+use Plugrbase\OpenAi\Enums\EngineType;
+
 class ApiEntity
 {
+    /**
+     * The default engine.
+     */
+    public string $engine = EngineType::DAVINCI;
+
     /**
      * The openApi object.
      */
@@ -14,8 +21,12 @@ class ApiEntity
      *
      * @return void
      */
-    public function __construct(object $openAi)
+    public function __construct(object $openAi, string $engine = null)
     {
         $this->openAi = $openAi;
+
+        if ($engine != '') {
+            $this->engine = $engine;
+        }
     }
 }
---
+++ src/Enums/EngineType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Plugrbase\OpenAi\Enums;
+
+class EngineType
+{
+    const DAVINCI = 'davinci';
+    const CURIE = 'curie';
+    const BABBAGE = 'babbage';
+    const ADA = 'ada';
+}
